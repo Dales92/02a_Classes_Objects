bb26bb47b3fa2390c5db56ac11daaa7daea6d123
David <dlesnik23@gmail.com>
2022-04-08 12:30:13 +0200

    Wertzuweisung / Felder

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,16 +3,33 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+        
+
         Cat cat = new Cat();
-        output("Blick von aussen: " + cat);
-        output("Blick von innen: " + cat.tellYourAddress());
+       
+        cat.name = "Grizabella";
+        cat.furColor = "white";
+        cat.age = 29;
+
+        output("Name: " + cat.name);
+        output("Fellfarbe: " + cat.furColor);
+        output("Alter " + cat.age);
+       // output(Integer.toString(cat.age));
+
         output("-----------------");
 
         Cat cat1 = new Cat();
-        output("Blick von aussen: " + cat1);
-        output("Blick von innen: " + cat1.tellYourAddress());
-        output("-----------------");
+        cat1.name = "Eminem";
+        cat1.furColor = "momsspagetthi";
+        cat1.age = 40;
+
+        output("Name: " + cat1.name);
+        output("Fellfarbe: " + cat1.furColor);
+        output("Alter " + cat1.age);
+        
+        //output("Blick von aussen: " + cat1);
+        //output("Blick von innen: " + cat1.tellYourAddress());
+        //output("-----------------");
 
 
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,6 +1,13 @@
 package com.cc.java;
 
 public class Cat {
+
+    public String name; 
+    public String furColor;
+    public int age;
+
+
+
     
     public Cat tellYourAddress() {
         return this; // Instanzvariable
